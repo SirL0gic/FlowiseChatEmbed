0a8c5d68180a3f5724ae4749f9ecb76426b693b3
Abis <abisali@hotmail.com>
2024-03-21 23:18:49 +0400

    Update

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -44,17 +44,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      Created by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://raen.ai'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span>Raen AI</span>
       </a>
     </span>
   );
